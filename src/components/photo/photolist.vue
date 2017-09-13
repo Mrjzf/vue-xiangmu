b56350b15343318caf36f322ff8110288eeebdd8
这很贾 <front_jzf@163.com>
2017-09-13 17:32:27 +0800

    图片分享

---
+++ src/components/photo/photolist.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="tmpl">
+        图片分享123
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -14,19 +14,21 @@ import home from './components/Home.vue';
 //import shopcar from './components/shopcar/car.vue';
 import newslist from './components/news/newslist.vue';
 import newsinfo from './components/news/newsinfo.vue';
+import photolist from './components/photo/photolist.vue';
 
 
 // 3.0.2 定义路由规则
 var router1 = new vueRouter({
-	linkActiveClass:'mui-active',  //改变路由激活时的class名称
+	linkActiveClass: 'mui-active',  //改变路由激活时的class名称
 	routes: [
-		{path:'/',redirect:'/home'},  //启动的时候  跳转到首页
-		{path:'/home',component:home},   //首页
+		{ path: '/', redirect: '/home' },  //启动的时候  跳转到首页
+		{ path: '/home', component: home },   //首页
 		//{path:'/shopcar',component:shopcar},  // 购物车
-		{path:'/news/newslist',component:newslist}, //新闻资讯列表页面
-		{path:'/news/newsinfo/:id',component:newsinfo}  // 新闻资讯详情页面 传入一个id
+		{ path: '/news/newslist', component: newslist }, //新闻资讯列表页面
+		{ path: '/news/newsinfo/:id', component: newsinfo },  // 新闻资讯详情页面 传入一个id
+		{ path: '/photo/photolist', component: photolist }
 	]
-	});
+});
 
 
 // 4.0 注册mint-ui
@@ -49,16 +51,16 @@ Vue.use(vueResource);
 
 // 8.0 定义一个全局过滤器实现日期的格式化
 import moment from 'moment';
-Vue.filter('datefmt',function(input,fmtstring){
-//	使用momentjs这个日期格式化类库实现日的格式化功能
+Vue.filter('datefmt', function (input, fmtstring) {
+	//	使用momentjs这个日期格式化类库实现日的格式化功能
 	return moment(input).format(fmtstring);
 });
 
 // 5.0 利用Vue对象进行解析渲染
 new Vue({
-	el:'#app',
+	el: '#app',
 	// 使用路由对象实例
-	router:router1,
+	router: router1,
 	// render:function(create){create(App)} //es5的写法
-	render:c=>c(App)  // es6的函数写法 =>：goes to
+	render: c => c(App)  // es6的函数写法 =>：goes to
 });
