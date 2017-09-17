43a584da3811e6d1117aedceb4f17f19cabeb445
这很贾 <front_jzf@163.com>
2017-09-17 09:19:51 +0800

    获取坐标

--- src/kits/offset.js
+++ src/kits/offset.js
@@ -2,13 +2,17 @@ export var carcount = document.querySelector('#carcount');
 //获取元素纵坐标
 export function getTop(e) {
     var offset = e.offsetTop;
-    if (e.offsetParent != null) offset += getTop(e.offsetParent);
+    if (e.offsetParent != null) {
+        offset += getTop(e.offsetParent);
+    }
     return offset;
 }
 
 //获取元素横坐标
 export function getLeft(e) {
     var offset = e.offsetLeft;
-    if (e.offsetParent != null) offset += getLeft(e.offsetParent);
+    if (e.offsetParent != null) {
+        offset += getLeft(e.offsetParent);
+    }
     return offset;
 }
